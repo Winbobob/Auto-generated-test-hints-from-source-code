8ffe20124467742370373a60644f628c36dbfdbd
Winbobob <zhu6@ncsu.edu>
2018-02-19 16:37:30 -0500

    Update script: 1. fix bugs; 2. add return values; 3. write auto-generated code to file.

---
+++ collusion_cycle_spec.rb
@@ -0,0 +1,90 @@
+describe CollusionCycle do
+  describe '.two_node_cycles' do
+    context 'when ap reviewers is include assignment_participant' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+    context 'when  assignment_participant reviews_by_reviewer ap is nil' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+    context 'when ap reviews_by_reviewer  assignment_participant is nil' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+  describe '.three_node_cycles' do
+    context 'when ap2 reviewers is include assignment_participant' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+    context 'when  assignment_participant reviews_by_reviewer ap1 is nil' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+    context 'when ap1 reviews_by_reviewer ap2 is nil' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+    context 'when ap2 reviews_by_reviewer  assignment_participant is nil' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+  describe '.four_node_cycles' do
+    context 'when ap3 reviewers is include assignment_participant' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+    context 'when  assignment_participant reviews_by_reviewer ap1 is nil' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+    context 'when ap1 reviews_by_reviewer ap2 is nil' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+    context 'when ap2 reviews_by_reviewer ap3 is nil' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+    context 'when ap3 reviews_by_reviewer  assignment_participant is nil' do
+      it 'returns collusion_cycles'
+        # Write your test here!
+
+    end
+
+  describe '.cycle_similarity_score' do
+    context 'when count == float 0.0' do
+      it 'returns similarity_score'
+        # Write your test here!
+
+    end
+
+  describe '.cycle_deviation_score' do
+    context 'when count == float 0.0' do
+      it 'returns deviation_score'
+        # Write your test here!
+
+    end
+
--- run.rb
+++ run.rb
@@ -13,7 +13,7 @@ KEY_WORDS = %i(
   next
 )
 
-def helper(res, level, test_skeleton, return_value)
+def helper(res, level, test_skeleton, nth_method, return_values)
   while res.length > 0
     if res.any? and res.first == :class
       level[0] = :class
@@ -29,24 +29,25 @@ def helper(res, level, test_skeleton, return_value)
       end
 
       if res.any? and res.first.first == :def
-        level[0] == :def
+        level[0] = :def
         test_skeleton[0] += ' ' * 2 + "describe '.#{res.first[1]}' do\n"
         # res = res.first
         res.first.shift # remove :def
         res.first.shift # remove method name
         res.first.shift # remove method params (?)
-        helper(res.first, level, test_skeleton, return_value)
-        level[0] == :def
+        nth_method[0] += 1
+        helper(res.first, level, test_skeleton, nth_method, return_values)
+        level[0] = :def
       end
 
       if res.any? and (res.first.first == :begin or res.first.first == :block)
         # res = res.first
         res.first.shift # remove :begin or :block
-        helper(res.first, level, test_skeleton, return_value)
+        helper(res.first, level, test_skeleton, nth_method, return_values)
       end
       
       if res.any? and res.first.first == :if
-        level[0] == :context
+        level[0] = :context
         test_skeleton[0] += ' ' * 4 + "context 'when "
         # res.first.flatten!
         res.first[1].flatten.each do |item|
@@ -57,16 +58,16 @@ def helper(res, level, test_skeleton, return_value)
         end
         test_skeleton[0].rstrip!
         test_skeleton[0] += "' do\n"
-        test_skeleton[0] += ' ' * 6 + "it 'returns #{return_value}'\n"
+        test_skeleton[0] += ' ' * 6 + "it 'returns [return_values_placeholder_#{nth_method[0]}]'\n"
         test_skeleton[0] += ' ' * 8 + "# Write your test here!\n\n"
         test_skeleton[0] += ' ' * 4 + "end\n\n"
         res.shift
         next
       end
 
-      if res.any? and res.first.first == :lvar and level[0] == :def
-        return_value[0] = res.first.last.to_s
-        puts "return value: #{return_value[0]}"
+      if res.any? and res.first.first == :lvar and level[0] == :context
+        return_values << res.first.last.to_s
+        # puts "return value: #{return_values[0]}"
         res.shift
         next
       end
@@ -84,9 +85,22 @@ res = Parser::CurrentRuby.parse(source_code) if source_code
 # puts res.to_sexp
 # puts res.to_sexp_array
 res = res.to_sexp_array
+nth_method = [-1]
 level = ['']
 test_skeleton = ['']
-return_value = ['']
-helper(res, level, test_skeleton, return_value)
-puts test_skeleton
-
+return_values = []
+helper(res, level, test_skeleton, nth_method, return_values)
+# replace [return value placeholder] with real return value
+return_values.each_with_index do |value, index|
+  test_skeleton[0].gsub!("[return_values_placeholder_#{index}]", value)
+end
+# puts test_skeleton
+spec_file_name = file_name[0].gsub!('.rb', '_spec.rb')
+puts "Generating #{spec_file_name}..."
+begin
+  file = File.open("spec/#{spec_file_name}", 'w')
+  file.write(test_skeleton[0])
+rescue IOError => e
+ensure 
+  file.close unless file.nil?
+end
